a5cacd2dfb94b38f5f54d16bdc18c43bc2d83fbc
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2018-06-09 22:57:35 -0400

    inquirer complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,13 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
-var dotenv = require('dotenv').config();
+require('dotenv').config();
+const {g, b, gr ,r ,y} = require("./console");
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.password, //FIX DOTENV SO YOU DON'T UPLOAD THIS
+    password: process.env.password, 
     database: "bamazon"
   });
   
@@ -21,34 +22,32 @@ var connection = mysql.createConnection({
   }*/
 
 function start() {
-    connection.query ("SELECT * FROM products", function(err, res){
+    connection.query ("SELECT * FROM products", function(err, res) {
       if(err) throw err;
-      console.log("Welcome! The following items are what we currently have in stock.");
+      console.log(res);
       console.log("       ");
-      console.log("ID" + " " + "PRODUCT" + "     " + "PRICE");
+      console.log(b("Welcome! The following items are what we currently have in stock."));
+      console.log("       ");
+      console.log(g("ID" + " " + "PRODUCT" + "     " + "PRICE"));
       for (i = 0; i<res.length; i++) {
-      console.log(res[i].id + ") " + res[i].product_name + ", " + "$ " + res[i].price);
+      console.log(res[i].id + ") " + res[i].product_name.toUpperCase() + " - " + "$ " + res[i].price);
       console.log("--------------------------------");
       }
-      toBuy();
+      inquirer
+      .prompt([
+        {
+          name: "item",
+          type: "input",
+          message: "What is the ID Number of the product you would like to buy?"
+        },
+        {
+          name: "unit",
+          type: "input",
+          message: "How many units would you like to buy?"
+        }
+      ])
+      .then(function(answer){
+        console.log(answer);
+      });
     });
-}
-
-function toBuy() {
-  inquirer
-  .prompt([
-    {
-      name: "item",
-      type: "input",
-      message: "What is the ID Number of the product you would like to buy?"
-    },
-    {
-      name: "unit",
-      type: "input",
-      message: "How many units would you like to buy?"
-    }
-  ])
-  .then(function(answer) {
-    console.log(answer);
-  });
-}
+  }
